07d0fe919d3daf6f246fd9236b120add89757cb3
zczqxc5 <146037962+zczqxc5@users.noreply.github.com>
2023-10-27 22:50:59 +0100

    Update Plant-Monitor.ino

--- Plant-Monitor.ino
+++ Plant-Monitor.ino
@@ -56,6 +56,78 @@ int value = 0;
 // Date and time
 Timezone GB;
 
+//extend function with buzzer and LED
+//中音NTF 0为空拍
+#define NTF0 -1
+#define NTF1 350
+#define NTF2 393	
+#define NTF3 441
+#define NTF4 495
+#define NTF5 556
+#define NTF6 624
+#define NTF7 661
+
+//高音NTFH
+#define NTFH1 700
+#define NTFH2 786
+#define NTFH3 882
+#define NTFH4 935
+#define NTFH5 965
+#define NTFH6 996
+#define NTFH7 1023
+
+//低音NTFL
+#define NTFL1 175
+#define NTFL2 196
+#define NTFL3 221
+#define NTFL4 234
+#define NTFL5 262
+#define NTFL6 294
+#define NTFL7 330
+
+//音符频率数组 
+int tune[]=
+{
+	NTF3,NTF3,NTF3,NTF3,NTF3,NTF3,
+	NTF3,NTF5,NTF1,NTF2,NTF3,NTF0,
+	NTF4,NTF4,NTF4,NTF4,NTF4,NTF3,NTF3,NTF3,NTF3,
+	NTF5,NTF5,NTF4,NTF2,NTF1,NTF0,
+
+	NTFL5,NTF3,NTF2,NTF1,NTFL5,NTF0,NTFL5,NTFL5,
+	NTFL5,NTF3,NTF2,NTF1,NTFL6,NTF0,
+	NTFL6,NTF4,NTF3,NTF2,NTFL7,NTF0,
+	NTF5,NTF5,NTF4,NTF2,NTF3,NTF1,NTF0,
+
+	NTFL5,NTF3,NTF2,NTF1,NTFL5,NTF0,
+	NTFL5,NTF3,NTF2,NTF1,NTFL6,NTF0,NTFL6,
+	NTFL6,NTF4,NTF3,NTF2,NTF5,NTF5,NTF5,NTF5,
+	NTF6,NTF5,NTF4,NTF2,NTF1,NTF0
+};
+//音符节拍数组
+float durt[]=
+{
+	0.5,0.5,1,0.5,0.5,1,
+	0.5,0.5,0.75,0.25,1.5,0.5,
+	0.5,0.5,1,0.5,0.5,0.5,0.5,0.25,0.25,
+	0.5,0.5,0.5,0.5,1.5,0.5,
+
+	0.5,0.5,0.5,0.5,1,0.5,0.25,0.25,
+	0.5,0.5,0.5,0.5,1,1,
+	0.5,0.5,0.5,0.5,1,1,
+	0.5,0.5,0.5,0.5,1,0.75,0.25,
+
+	0.5,0.5,0.5,0.5,1,1,
+	0.5,0.5,0.5,0.5,1,0.5,0.5,
+	0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+	0.5,0.5,0.5,0.5,0.75,0.25
+};
+const int TrigPin = 15;
+const int EchoPin = 16;
+float cm;
+//定义蜂鸣器引脚，音符长度变量
+int buzzer_pin = 14;
+int length;
+int ledp=2;
 
 
 void setup() {
@@ -71,6 +143,15 @@ void setup() {
   digitalWrite(sensorVCC, LOW);
   pinMode(blueLED, OUTPUT); 
   digitalWrite(blueLED, HIGH);
+
+  //set up LED & buzzer &  ultrasonic sensor
+  pinMode(buzzer_pin, OUTPUT);
+  length = sizeof(tune)/sizeof(tune[0]);
+
+  pinMode(TrigPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
+  pinMode(ledp, OUTPUT);
+
 
   // open serial connection for debug info
   Serial.begin(115200);
@@ -92,6 +173,31 @@ void setup() {
 }
 
 void loop() {
+    digitalWrite(14, LOW);
+
+  digitalWrite(TrigPin, LOW); //低高低电平发一个短时间脉冲去TrigPin
+  delayMicroseconds(2);
+  digitalWrite(TrigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TrigPin, LOW);
+
+
+  cm = pulseIn(EchoPin, HIGH) / 58.0; //将回波时间换算成cm
+  cm = (int(cm * 100.0)) / 100.0; //保留两位小数
+  if (cm>=2 && cm<=10 && readMoisture()>100)
+    //for循环演奏曲子
+    for(int x=0;x<length;x++)
+    {
+      tone(buzzer_pin, tune[x]);
+      digitalWrite(ledp,HIGH);
+      delay(500*durt[x]);	
+      digitalWrite(ledp,LOW);	
+      delay(100*durt[x]);				//这里的500为控制每个音符的时长来定曲子的节奏
+      noTone(buzzer_pin);
+    }
+    delay(500);								//开始下一轮循环的时间间隔
+
+
   // handler for receiving requests to webserver
   server.handleClient();
 
@@ -104,7 +210,7 @@ void loop() {
   client.loop();
 }
 
-void readMoisture(){
+float readMoisture(){  //need return
   
   // power the sensor
   digitalWrite(sensorVCC, HIGH);
@@ -117,6 +223,8 @@ void readMoisture(){
   delay(100);
   Serial.print("Wet ");
   Serial.println(Moisture);   // read the value from the nails
+ return Moisture;  //get the value of mositure
+
 }
 
 void startWifi() {
